ae4c2aa7d3d41f8f1d922d15b7cb2c890d58a38e
Yuichi Murata <yuichi1004@gmail.com>
2014-10-16 19:44:31 +0900

    Fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='kanc',
-    version='1.0',
+    version='0.1.0',
     packages=['kanc', 'kanc.command'],
     install_requires = ['tabulate'],
     scripts=['scripts/kanc'],
