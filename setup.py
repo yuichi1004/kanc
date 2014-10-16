f544cf7274680c11d4aece977ca3a3a1cc58a2e3
Yuichi Murata <yuichi1004@gmail.com>
2014-10-16 20:46:28 +0900

    Add parameters for setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='kanc',
+    description='Simple Kanboard CUI client',
     version='0.1.0',
+    author="Yuichi Murata",
+    author_email="yuichi1004@gmail.com",
+    url = "https://github.com/yuichi1004/kanc",
+    download_url = "https://github.com/yuichi1004/kanc/archive/v0.1.0.tar.gz",
     packages=['kanc', 'kanc.command'],
-    install_requires = ['tabulate'],
+    install_requires = ['tabulate >= 0.7'],
     scripts=['scripts/kanc'],
+    license="MIT"
     )
 
