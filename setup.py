8396c79e2d9adde47206e9c1b2fe1098d6a2b660
Yuichi Murata <yuichim@buffalotech.com>
2014-10-27 12:38:24 +0900

    v.0.1.3

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='kanc',
     description='Simple Kanboard CUI client',
-    version='0.1.2',
+    version='0.1.3',
     author="Yuichi Murata",
     author_email="yuichi1004@gmail.com",
     url = "https://github.com/yuichi1004/kanc",
-    download_url = "https://github.com/yuichi1004/kanc/archive/v0.1.2.tar.gz",
+    download_url = "https://github.com/yuichi1004/kanc/archive/v0.1.3.tar.gz",
     packages=['kanc', 'kanc.command'],
     install_requires = ['tabulate >= 0.7'],
     scripts=['scripts/kanc'],
